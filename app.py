d6449983dc30c362d31042bc56390750f89853a3
saadmdsabah <103499208+saadmdsabah@users.noreply.github.com>
2023-10-07 09:38:09 +0530

    app.py

--- app.py
+++ app.py
@@ -29,5 +29,5 @@ def login():
    return render_template("index.html", y = "The predicted profit is  "+str(np.round(output[0])))
 
 if __name__ == "__main__" :
-    app.run(debug=True)
+    app.run(debug=False,host="0.0.0.0")
     
